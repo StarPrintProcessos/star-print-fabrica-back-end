8cc23f506f7d255e94b9021bc8013f89ac4a6515
StarPrintProcessos <processos@starprintonline.com.br>
2025-10-15 09:16:59 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
@@ -31,7 +32,8 @@ async function bootstrap() {
 
   // Em produção, escuta em todas as interfaces (0.0.0.0)
   // Isso é crucial para contêineres Docker
-  const port = process.env.BACK_END_PORT ?? 3000;
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 3000;
   await app.listen(port, '0.0.0.0');
   console.log(`Aplicação rodando na porta ${port}`);
   console.log(`Documentação Swagger disponível em: http://localhost:${port}/api`);
